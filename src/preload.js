25e2ae42917cf9c2fded6a86bd1943498c6af53a
mpaulweeks <mpaulweeks@gmail.com>
2021-09-18 19:49:10 -0400

    skip devtools

--- src/preload.js
+++ src/preload.js
@@ -4,7 +4,7 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
 window.addEventListener('DOMContentLoaded', () => {
-  console.log('preload.js');
+  console.log('running preload...');
   for (const dependency of ['chrome', 'node', 'electron']) {
     console.log(`${dependency}-version`, process.versions[dependency]);
   }
